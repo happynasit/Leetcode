9008951df469d29ee886587143fe2afdd38ebc62
happy nasit <happynasit@gmail.com>
2022-06-10 11:27:07 -0400

    Completed Easy: Q125. Valid Palindrome

---
+++ strings/125. Valid Palindrome.py
@@ -0,0 +1,14 @@
+def isPalindrome(self, s: str) -> bool:
+    """
+    https://leetcode.com/problems/valid-palindrome/
+    Easy: 125. Valid Palindrome
+    """
+    ss = ''
+    for i in s:
+        if i.isalnum():
+            ss = ss + i
+    # ss is the lower case without any spaces
+    ss = ss.lower()
+    # to check whether or not it is a pallindrome
+    return ss == ss[len(ss) :: -1]
+        

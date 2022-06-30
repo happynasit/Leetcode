7fed8e0ac7772f762f718b2dc3063f2dc05fd41b
happynasit <happynasit@gmail.com>
2022-06-30 15:16:54 -0400

    completed

--- strings/67. Add Binary.py
+++ strings/67. Add Binary.py
@@ -1,3 +1,2 @@
 def addBinary(self, a: str, b: str) -> str:
-    """Completed. Q67: Given two binary strings a and b, return their sum as a binary string."""
-    
+    """Completed. Q67: Given two binary strings a and b, return their sum as a string."""

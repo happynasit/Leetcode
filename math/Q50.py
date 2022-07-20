f57c842bb8d3469d3419153f170f58129688e3d3
happynasit <happynasit@gmail.com>
2022-07-20 11:21:38 -0400

    Completed Q50. Pow(x, n)

---
+++ math/Q50.py
@@ -0,0 +1,10 @@
+def myPow(self, x: float, n: int) -> float:
+    """
+    Completed MEDIUM: 
+    Implement pow(x, n), which calculates x raised to the power n (i.e., x^n).
+
+    """
+    if n >= 0:
+        return x ** n
+    elif n < 0:
+        return x ** n

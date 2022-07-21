1a6ea8975e902e8467a8d9b71464d8820d5cd152
happynasit <happynasit@gmail.com>
2022-07-21 15:23:25 -0400

    Completed EASY Q367. Valid Perfect Square

---
+++ math/Q367.py
@@ -0,0 +1,17 @@
+def isPerfectSquare(num: int) -> bool:
+    """
+    Completed EASY:
+    Given a positive integer num, write a function which returns True 
+    if num is a perfect square else False.
+
+    Follow up: Do not use any built-in library function such as sqrt.
+    """
+    if num < 0:
+        return False
+    else:
+        value = num ** 0.5
+        if value - int(value) == 0:
+            return True
+        else:
+            return False
+        

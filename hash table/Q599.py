dd103a39bb267f39627a9a88c0031d46da9ee84d
happynasit <happynasit@gmail.com>
2022-08-08 17:40:49 +0300

    Completed EASY: Minimum Index Sum of Two Lists

---
+++ hash table/Q599.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findRestaurant(self, list1: list[str], list2: list[str]) -> list[str]:
+        """
+        https://leetcode.com/problems/minimum-index-sum-of-two-lists/
+
+        Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of favorite restaurants 
+        represented by strings.
+
+        You need to help them find out their common interest with the least list index sum. If there is a choice tie between 
+        answers, output all of them with no order requirement. You could assume there always exists an answer. 
+        """
+        restaurants = {}
+        
+        for res in list1:
+            if res in list2:
+                restaurants[res] = list1.index(res) + list2.index(res)
+        
+        lst = []
+        for restaurant in restaurants:
+            if restaurants[restaurant] == min(restaurants.values()):
+                lst.append(restaurant)
+        return lst

067bcfd29db917cd75f24abdd6df941a799d7d50
happynasit <happynasit@gmail.com>
2022-08-10 18:10:36 +0300

    completed

---
+++ hash table/Q804.py
@@ -0,0 +1,25 @@
+class Solution:
+    def uniqueMorseRepresentations(self, words: list[str]) -> int:
+        """
+
+
+        Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+        Return the number of different transformations among all words we have.
+
+        """
+        alpha = {'a': ".-", 'b': "-...", 'c': "-.-.", 'd': "-..", 'e': ".",
+                'f': "..-.", 'g': "--.", 'h': "....", 'i': "..", 'j': ".---", 
+                'k': "-.-", 'l': ".-..", 'm': "--", 'n': "-.", 'o': "---", 
+                 'p': ".--.", 'q': "--.-", 'r': ".-.", 's': "...", 't': "-",
+                'u': "..-", 'v': "...-", 'w': ".--", 'x': "-..-", 'y': "-.--",
+                'z': "--.."}
+        
+        set_morse = set()
+        for word in words:
+            morse = ''
+            
+            for letter in word:
+                morse = morse + alpha[letter]
+            set_morse.add(morse)
+            
+        return len(set_morse)

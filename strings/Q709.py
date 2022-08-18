d8fa1b7863d962765a5dae18c893c7549eaf918c
happynasit <105992603+happynasit@users.noreply.github.com>
2022-08-18 14:43:29 +0300

    Completed EASY: To Lower Case

---
+++ strings/Q709.py
@@ -0,0 +1,10 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+      """
+      https://leetcode.com/problems/to-lower-case/
+      
+      Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+
+      """
+        return s.lower()

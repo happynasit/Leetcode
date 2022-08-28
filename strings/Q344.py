edf57f6be6e2d0104fac610c6464b23cdcdfd5f6
happynasit <105992603+happynasit@users.noreply.github.com>
2022-08-28 14:42:10 +0300

    Completed EASY: Reverse String

--- strings/Q344.py
+++ strings/Q344.py
@@ -1,5 +1,7 @@
 def reverseString(self, s: List[str]) -> None:
   """
+  https://leetcode.com/problems/reverse-string/ 
+  
   Completed Easy: Q344. reverse strings in a list
   """
   rever_str = list.reverse(s)

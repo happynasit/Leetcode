8b8493d2c732165110c29a513a2e5d0c611ca797
happynasit <105992603+happynasit@users.noreply.github.com>
2022-08-28 15:35:31 +0300

    Completed EASY: Perfect Number

--- math/Q507.py
+++ math/Q507.py
@@ -6,7 +6,7 @@ class Solution:
       A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number 
       itself. A divisor of an integer x is an integer that can divide x evenly.
 
-      Given an integer n, return true if n is a perfect number, otherwise return false.
+      Given an integer n, return true if n is a perfect number, or else return false.
       """
         if num <= 1:
             return False

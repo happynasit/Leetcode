ee5991ef81a43b47ef83d48b40a9afa8953e8854
happynasit <105992603+happynasit@users.noreply.github.com>
2022-09-05 10:32:21 -0400

    Completed EASY: Single number

--- array/Q136.py
+++ array/Q136.py
@@ -1,6 +1,8 @@
 def singleNumber(nums: List[int]) -> int:
     """
-    Completed EASY: 
+    https://leetcode.com/problems/single-number/
+    
+    
     Given a non-empty array of integers nums, every element appears twice 
     except for one. Find that single one.
 

15ee2eefabec9283fb365782e1130ad88a0e56ad
happynasit <105992603+happynasit@users.noreply.github.com>
2023-05-12 15:58:43 +0300

    Completed EASY: Postorder Traversal

---
+++ binary tree/Q145.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        """
+        Given the root of a binary tree, return the postorder traversal of its nodes' values.
+        """
+        l = []
+
+        if root is not None:
+            l.extend(self.postorderTraversal(root.left))
+            l.extend(self.postorderTraversal(root.right))
+            l.append(root.val)
+
+        return l
+
+
+
+

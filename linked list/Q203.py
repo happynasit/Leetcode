72e7e8814a00910d3e2aa2c1f7003af7a322c413
happynasit <105992603+happynasit@users.noreply.github.com>
2023-05-22 22:22:40 +0300

    Completed EASY: Remove Linked List Elements

--- linked list/Q203.py
+++ linked list/Q203.py
@@ -5,6 +5,9 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        """
+        Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+        """
         curr = head
         prev = head
         if head is not None:

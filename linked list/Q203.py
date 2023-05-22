b01b960dc27bdcebbba406dbfac940088fd64e14
happynasit <happynasit@gmail.com>
2023-05-22 22:21:32 +0300

    Completed EASY: Remove all occurences of an element

---
+++ linked list/Q203.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        curr = head
+        prev = head
+        if head is not None:
+            if head.val == val:
+                head = head.next
+                curr = head
+            else:
+                while curr is not None:
+                    if curr.val == val:
+                        prev.next = curr.next
+                        curr = curr.next
+                    else:
+                        prev = curr
+                        curr = curr.next
+            return head

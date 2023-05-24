bc6988efadbca2f0865a20e00c33c4dc3281b948
happynasit <happynasit@gmail.com>
2023-05-24 15:11:36 +0300

    Completed MEDIUM: Permutations

---
+++ math/Q46.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        """
+        Given an array nums of distinct integers, return all the possible permutations. 
+        You can return the answer in any order.
+        """
+        l = []
+        if len(nums) == 1:
+            return [nums.copy()]
+        
+        # Using recursion
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            sub_ = self.permute(nums)
+
+            for value in sub_:
+                value.append(n)
+
+            l.extend(sub_)
+            nums.append(n)
+        return l

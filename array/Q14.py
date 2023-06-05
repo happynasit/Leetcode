45ecaf691df8350a350856f8e468742a03248141
happynasit <105992603+happynasit@users.noreply.github.com>
2023-06-05 22:11:27 +0300

    Completed EASY: Common Prefix

--- array/Q14.py
+++ array/Q14.py
@@ -1,7 +1,5 @@
 def longestCommonPrefix(strs: List[str]) -> str:
       """
-      Completed EASY:
-      
       Write a function to find the longest common prefix string amongst an array of strings.
       If there is no common prefix, return an empty string ""
       """

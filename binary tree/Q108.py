be756e724fd97de7dfe5227f088acd70cdc8aa59
happynasit <105992603+happynasit@users.noreply.github.com>
2023-07-01 21:59:55 +0300

    Completed EASY: Convert ordered array to Binary Search Tree

---
+++ binary tree/Q108.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+      """
+      
+      Given an integer array nums where the elements are sorted in ascending order, convert it to a 
+      height-balanced binary search tree.
+      """
+        if nums == []:
+            return None
+        else:
+            mid = len(nums) // 2
+            root = TreeNode(nums[mid])
+
+            # we have the middle element that can be set as the root of the tree
+
+            # Recursion
+            root.left = self.sortedArrayToBST(nums[:mid])
+            root.right = self.sortedArrayToBST(nums[mid+1:])
+
+            return root

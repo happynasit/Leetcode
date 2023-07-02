e6a8a43d1c76846decfbdd4f961634cf11fa02dd
happynasit <105992603+happynasit@users.noreply.github.com>
2023-07-02 23:00:13 +0300

    Completed EASY: Balanced Binary Search Tree

--- binary tree/Q108.py
+++ binary tree/Q108.py
@@ -7,7 +7,6 @@
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
       """
-      
       Given an integer array nums where the elements are sorted in ascending order, convert it to a 
       height-balanced binary search tree.
       """

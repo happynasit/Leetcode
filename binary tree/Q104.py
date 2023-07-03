be69ba6791f756c7306da9087d0e1256fb80f970
happynasit <105992603+happynasit@users.noreply.github.com>
2023-07-03 21:58:10 +0300

    Completed EASY: Maximum Depth of a BST

---
+++ binary tree/Q104.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+      """
+      Given the root of a binary tree, return its maximum depth.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+      """
+        if root is None:
+            return 0
+        else:
+            depth_left = self.maxDepth(root.left)
+            depth_right = self.maxDepth(root.right)
+
+            if depth_left > depth_right:
+                return depth_left + 1
+            else:
+                return depth_right + 1

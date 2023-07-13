ad8e5760830e6c2caac40ed6638f6b72b513b1cc
happynasit <105992603+happynasit@users.noreply.github.com>
2023-07-13 15:12:46 +0300

    Completed EASY: Majority element of a list

--- array/Q169.py
+++ array/Q169.py
@@ -3,10 +3,6 @@ def majorityElement(nums: list[int]) -> int:
     Completed EASY:
     Given an array nums of size n, return the majority element.
 
-    The majority element is the element that appears more than 
-    ⌊n / 2⌋ times. You may assume that the majority element always 
-    exists in the array.
-
     """
     new_lst = []
         

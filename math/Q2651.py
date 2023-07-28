d0f3e367d0b37c320c7b183a1e996930b607a797
happynasit <105992603+happynasit@users.noreply.github.com>
2023-07-28 15:06:16 +0300

    Completed EASY: Find the delayed arrival Time

--- math/Q2651.py
+++ math/Q2651.py
@@ -1,9 +1,6 @@
 class Solution:
     def findDelayedArrivalTime(self, arrivalTime: int, delayedTime: int) -> int:
       """
-      You are given a positive integer arrivalTime denoting the arrival time of a 
-      train in hours, and another positive integer delayedTime denoting the amount 
-      of delay in hours.
       """
         final_time = arrivalTime + delayedTime
         if final_time < 24:

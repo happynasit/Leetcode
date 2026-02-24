fe2c3f5fb8ab1bc692e0b89b2f16270b1adba7eb
happynasit <105992603+happynasit@users.noreply.github.com>
2026-02-23 23:04:04 -0500

    Update ContainsDuplicate.py

--- Neetcode150/ContainsDuplicate.py
+++ Neetcode150/ContainsDuplicate.py
@@ -6,6 +6,7 @@ simply check the length of the list vs the set.
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         # there is no fastest way of doing this
+        
         # WORST CASE IF THERE IS AN ARRAY WITH NO REPEATING ELEMENTS, WE HAVE TO
       # ITERATE N TIMES THROUGHOUT THE LIST TO SEE IF THE LIST HAS ANY REPEATING ELEMENT.
       # THATS WHY THE BEST RUNTIME IS LINEAR
